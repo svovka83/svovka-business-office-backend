3218fd08474326159a4e287b60910833056a617c
Volodymyr Smoliak <svovka83@gmail.com>
2024-05-16 10:09:44 +0300

    second commit

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+
+mongoose
+  .connect(
+    "mongodb+srv://svovka83:WRbG9BycwD5FwSfb@cluster0.rgue1es.mongodb.net/kids?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => {
+    console.log("DB connecting");
+  })
+  .catch((err) => {
+    console.log("DB error", err);
+  });
 
 const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.json({

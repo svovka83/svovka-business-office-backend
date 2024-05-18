9ccabdb2575f75048d43eca8f4543a5ab14a2182
Volodymyr Smoliak <svovka83@gmail.com>
2024-05-19 02:09:31 +0300

    deploy

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import cors from "cors";
 import { register, getAllUsers } from "./controller/UserController.js";
 
 mongoose
-  .connect("mongodb+srv://svovka83:WRbG9BycwD5FwSfb@cluster0.rgue1es.mongodb.net/addition?retryWrites=true&w=majority&appName=Cluster0")
+  .connect(process.env.MONGODB_URI || "mongodb+srv://svovka83:WRbG9BycwD5FwSfb@cluster0.rgue1es.mongodb.net/addition?retryWrites=true&w=majority&appName=Cluster0")
   .then(() => {
     console.log("DB connecting");
   })
